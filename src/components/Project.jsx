640a979f7e0e95ba3bf148f3e5868fa9f4661d7f
Olga Felczak <ofelczak@gmail.com>
2023-03-06 22:58:08 +0000

    Added projects. Updated photo in the footer.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,21 @@
 import { Col } from "react-bootstrap";
 
-export const Project = ({ title, description, imgUrl }) => {
+export const Project = ({ title, imgUrl, githubUrl, liveUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
+          <a href={githubUrl} target="_blank">
+            GitHub
+          </a>
+          <br />
+          {liveUrl && (
+            <a href={liveUrl} target="_blank">
+              Live URL
+            </a>
+          )}
         </div>
       </div>
     </Col>
